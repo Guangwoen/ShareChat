20da5c47af5cf2f8ec9ceeb5cae5d27a4435000f
guangW <1822140986@qq.com>
2022-12-09 19:06:40 +0800

    websocket change

--- src/main/java/sharechat/com/websocket/WebSocketService.java
+++ src/main/java/sharechat/com/websocket/WebSocketService.java
@@ -1,57 +1,56 @@
 package sharechat.com.websocket;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpSession;
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@ServerEndpoint(value="/websocket", configurator = EndpointConfigure.class)
+@ServerEndpoint(value="/websocket/{sender}/2/{receiver}", configurator = EndpointConfigure.class)
 public class WebSocketService {
     private static int onlineCount = 0;
 
-    private static final CopyOnWriteArraySet<WebSocketService> webSocketServices = new CopyOnWriteArraySet<WebSocketService>();
-
-    private Session session;
+    private static final ConcurrentHashMap<String, Session> webSocketServices
+            = new ConcurrentHashMap<>();
 
     String str = "";
 
     @OnOpen
-    public void onOpen(Session session, EndpointConfig config) {
-        this.session = session;
-        HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
-        webSocketServices.add(this);
+    public void onOpen(@PathParam("sender") String sender,
+                       @PathParam("receiver") String receiver,
+                       Session session,
+                       EndpointConfig config) {
+        webSocketServices.put(sender+'|'+receiver, session);
         addOnlineCount();
-
-        try {
-            sendMessage("服务端链接成功");
-        }
-        catch (IOException e) {
-            System.out.println("IO异常");
-        }
+        System.out.println("新连接: " + sender + ", 当前在线人数为:" + getOnlineCount());
     }
 
     @OnClose
-    public void onClose() {
-        webSocketServices.remove(this);
+    public void onClose(@PathParam("sender") String sender,
+                        @PathParam("receiver") String receiver) {
+        webSocketServices.remove(sender+'|'+receiver);
         subOnlineCount();
-
-        System.out.println("一个链接关闭！当前在线人数为:" + getOnlineCount());
+        System.out.println("一个链接关闭！"+sender+", 当前在线人数为:" + getOnlineCount());
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) {
-        System.out.println("来自客户端的消息:" + message);
-        str = message;
+    public void onMessage(@PathParam("sender") String sender,
+                          @PathParam("receiver") String receiver,
+                          String message,
+                          Session session) {
+        System.out.println("来自客户端" + sender + "的消息:" + message);
+
+        // 数据库查找
         // 向数据库保存信息
 
-        System.out.println("onMessage sessionId is:" + session.getId());
+
+
         try{
-            sendMessage("OK");
+            sendMessage(message, sender, receiver);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,31 +58,34 @@ public class WebSocketService {
     }
 
     @OnError
-    public void onError(Session session, Throwable error) {
-        for (WebSocketService item : webSocketServices) {
-            try {
-                item.sendMessage("响应超时!");
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        error.printStackTrace();
+    public void onError(Session session, Throwable error) throws IOException {
+        sendInfo("响应超时");
     }
 
-    public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText("服务端信息:" + message);
+    /**
+     * 发送消息
+     * @param message 消息本体
+     * @param sender 消息发送者
+     * @param receiver 消息接收对象
+     * */
+    public void sendMessage(String message, String sender, String receiver) throws IOException {
+        webSocketServices.get(receiver+'|'+sender)
+                .getBasicRemote().sendText("来自"+sender+"的信息:" + message);
     }
 
-    public static void sendInfo(String message) throws IOException {
-        for (WebSocketService item : webSocketServices) {
+    /**
+     * 群发消息
+     * */
+    public void sendInfo(String message) throws IOException {
+        Collections.list(webSocketServices.keys()).forEach( item -> {
+            String[] users = item.split("\\|");
             try {
-                item.sendMessage(message);
+                sendMessage(message, users[0], users[1]);
             }
             catch (IOException e) {
-                continue;
+                return;
             }
-        }
+        });
     }
 
     public static synchronized int getOnlineCount() {
